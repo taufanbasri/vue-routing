eb6619637154185ceef0210d1e5273213d570bdf
Taufan Basri <taufanprasetyobasri@gmail.com>
2022-05-31 23:45:22 +0700

    Route Leave Guards

--- src/components/users/UsersList.vue
+++ src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
   <button @click="confirmInput">Confirm</button>
+  <button @click="saveChanges">Save Changes</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -13,17 +14,39 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  data() {
+    return {
+      changesSaved: false
+    }
+  },
   methods: {
     confirmInput() {
       this.$router.push('/teams')
+    },
+    saveChanges() {
+      this.changesSaved = true
     }
   },
-  beforeRouteEnter (to, from, next) {
+  beforeRouteEnter(to, from, next) {
     console.log('UsersList component beforeRouteEnter');
     console.log(to, from);
 
     next()
-  }
+  },
+  beforeRouteLeave(to, from, next) {
+    console.log('UsersList component beforeRouteLeave');
+    console.log(to, from);
+    
+    if (this.changesSaved) {
+      next()
+    } else {
+      const userWantsToLeave = confirm('Are you sure? You got unsaved changes!');
+      next(userWantsToLeave)
+    }
+  },
+  unmounted() {
+    console.log('unmounted');
+  },
 };
 </script>
 

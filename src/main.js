470f74fddb8a1473feed7470c4149158cd2eaab3
Taufan Basri <taufanprasetyobasri@gmail.com>
2022-05-31 14:14:50 +0700

    Multiple routes view

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import TeamsList from './components/teams/TeamsList';
 import UsersList from './components/users/UsersList';
 import TeamMembers from './components/teams/TeamMembers';
 import NotFound from './components/NotFound';
+import TeamsFooter from './components/teams/TeamsFooter';
+import UsersFooter from './components/users/UsersFooter';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -14,7 +16,10 @@ const router = createRouter({
     {
       name: 'teams',
       path: '/teams',
-      component: TeamsList,
+      components: {
+        default: TeamsList,
+        footer: TeamsFooter,
+      },
       children: [
         {
           name: 'teams-member',
@@ -24,7 +29,10 @@ const router = createRouter({
         },
       ],
     },
-    { path: '/users', component: UsersList },
+    { path: '/users', components: {
+      default: UsersList,
+      footer: UsersFooter
+    } },
     { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active',
